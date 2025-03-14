c5cbddfde4d68c4bea0c6d8f5dc580d7714141db
Salsaazz <salwa.azzouz33@gmail.com>
2025-03-14 14:35:11 +0100

    fix bug

--- MyCloset/MyCloset.Frontend.MAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyCloset.Frontend.MAUI.AssemblyInfo.cs
+++ MyCloset/MyCloset.Frontend.MAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MyCloset.Frontend.MAUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCloset.Frontend.MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f7b370b5e00bac6391c973a1459305a8f184d7c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+410c0a291f27579a1a39976a0ec93b4a27e1cb4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCloset.Frontend.MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCloset.Frontend.MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
